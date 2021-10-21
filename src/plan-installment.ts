7a18e769061b6d2fa718f9f08515db5c3f33da07
shinchven <shinchven@gmail.com>
2021-10-22 01:17:13 +0800

    modify variable names

--- src/plan-installment.ts
+++ src/plan-installment.ts
@@ -1,21 +1,46 @@
 const planInstallment = (amountPerInstallment: number, installments: number, annualizedRateOfReturn: number) => {
-    let estimatedReturnAmount = 0;
 
+
+    const earningsOfEachInstallment = new Array<number>();
+
+    // 累加每期可通过理财获得的回报
     for (let i = 0; i < installments; i++) {
-        const returnPerInstallment = (amountPerInstallment * annualizedRateOfReturn / 12 * i).toFixed(2)
-        estimatedReturnAmount += Number(returnPerInstallment);
+        /**
+         * 每期可通过理财获得的回报
+         */
+        const earningOfInstallment =
+            Number((
+                // 每期金额 * 年化利率 / 12 * 每期金额可参与理财的月份
+                amountPerInstallment * annualizedRateOfReturn / 12 * i
+            ).toFixed(2)) // 小数点2位
+
+        earningsOfEachInstallment.push(earningOfInstallment);
     }
 
-    const total = amountPerInstallment * installments;
-    const estimatedTotalAfterReturned = total - estimatedReturnAmount
+    /**
+     * 总支付
+     */
+    const totalPayment = amountPerInstallment * installments;
+
+
+    /**
+     * 通过理财可获得的总回报
+     */
+    let totalEarningFromInstallment = earningsOfEachInstallment.reduce((a, b) => a + b, 0);
+
+    /**
+     * 预计理财获得回报后的支出
+     */
+    const estimatedActualSpending = totalPayment - totalEarningFromInstallment
 
     return {
-        estimatedReturnAmount,
-        estimatedTotalAfterReturned,
-        total,
         amountPerInstallment,
         installments,
+        totalPayment,
         annualizedRateOfReturn,
+        totalEarningFromInstallment,
+        estimatedActualSpending,
+        earningsOfEachInstallment
     }
 }
 
